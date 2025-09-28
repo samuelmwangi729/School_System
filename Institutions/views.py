42de739f8347e7f6e30c35aa53199e4100dece11
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-28 05:58:27 +0300

    completed

--- Institutions/views.py
+++ Institutions/views.py
@@ -1,7 +1,8 @@
 from .serializers import institutionSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from rest_framework import response,status
-class InstitutionView(ListAPIView):
+from Institutions.models import Institution
+class InstitutionView(APIView):
     serializer_class = institutionSerializer
 
     def post(self,request):
@@ -12,3 +13,9 @@ class InstitutionView(ListAPIView):
             serializer.save()
             return response.Response(serializer.data,status=status.HTTP_201_CREATED)
         return response.Response({"errors":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self,request):
+        queryset = Institution.objects.all()
+        serializer = institutionSerializer(queryset,many=True)
+        return response.Response(serializer.data,status=status.HTTP_200_OK)
+        
