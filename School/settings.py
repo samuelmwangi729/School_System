3e946ec5d1091692d54c1cef5dd96ff9a37c79a1
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-13 07:10:58 +0300

    feature:added postgres to the db

--- School/settings.py
+++ School/settings.py
@@ -132,8 +132,12 @@ WSGI_APPLICATION = 'School.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'school',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
